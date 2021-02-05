f7ff70db5131fccc99422f1ee4ae6ab039ea1fce
Kilo Loco <kyle@kiloloco.com>
2021-02-04 17:34:02 -0800

    day04-flashcards start

---
+++ day04_flashcards/android/app/src/main/kotlin/com/example/day04_flashcards/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.day04_flashcards
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ day04_flashcards/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
