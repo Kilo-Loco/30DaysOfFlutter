07656940c293a47df1745374d7985dd41eea5f3a
Kilo Loco <kyle@kiloloco.com>
2021-02-06 21:37:52 -0800

    day06-picture-carousel

--- day06_picture_carousel/lib/main.dart
+++ day06_picture_carousel/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,8 +11,30 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  final _imagePaths = [
+    'images/day01.png',
+    'images/day02.png',
+    'images/day03.png',
+    'images/day04.png',
+    'images/day05.png',
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Scaffold());
+    return MaterialApp(
+        home: Scaffold(
+      body: SafeArea(
+        child: CarouselSlider(
+            options: CarouselOptions(autoPlay: true),
+            items: _imagePaths.map((imagePath) {
+              return Builder(builder: (context) {
+                return Container(
+                    width: MediaQuery.of(context).size.width,
+                    margin: EdgeInsets.symmetric(horizontal: 4),
+                    child: Image.asset(imagePath));
+              });
+            }).toList()),
+      ),
+    ));
   }
 }
