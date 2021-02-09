93338c8422fa5ac9b45116c8d3a6fa5ace119e08
Kilo Loco <kyle@kiloloco.com>
2021-02-08 20:37:27 -0800

    day08-user-settings start

---
+++ day08_user_settings/android/app/src/main/kotlin/com/example/day08_user_settings/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.day08_user_settings
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ day08_user_settings/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(),
+    );
+  }
+}
