57a59362a2dc03bbada61743c9209308caca0f13
Kilo Loco <kyle@kiloloco.com>
2021-02-12 21:09:57 -0800

    day12-dynamic-lists-and-grids finished

--- day12_dynamic_lists_and_grids/lib/main.dart
+++ day12_dynamic_lists_and_grids/lib/main.dart
@@ -10,10 +10,42 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  final _users = List.filled(100, "kilo loco");
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(backgroundColor: Colors.white70, appBar: AppBar()),
+      home: DefaultTabController(
+          length: 2,
+          child: Scaffold(
+            backgroundColor: Colors.white70,
+            appBar: AppBar(
+              bottom: TabBar(tabs: [Text('List'), Text('Grid')]),
+            ),
+            body:
+                TabBarView(children: [_contentListView(), _contentGridView()]),
+          )),
     );
+  }
+
+  Widget _contentListView() {
+    return ListView.builder(
+      itemCount: _users.length,
+      itemBuilder: (context, index) => Card(
+        child: ListTile(
+          title: Text(index.toString()),
+          onTap: () => print("hello $index"),
+        ),
+      ),
+    );
+  }
+
+  Widget _contentGridView() {
+    return GridView.builder(
+        itemCount: _users.length,
+        gridDelegate:
+            SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
+        itemBuilder: (context, index) =>
+            Card(child: GridTile(child: Center(child: Text(_users[index])))));
   }
 }
