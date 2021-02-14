d3eb6bf1ed73faeb4fa7ba1194e5f100671d89fd
Kilo Loco <kyle@kiloloco.com>
2021-02-13 21:38:33 -0800

    flutter-bloc-and-cubit starter

---
+++ flutter_bloc_and_cubit/android/app/src/main/kotlin/com/example/flutter_bloc_and_cubit/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_bloc_and_cubit
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ flutter_bloc_and_cubit/lib/posts_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class PostsView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Posts'),
+      ),
+    );
+  }
+}
