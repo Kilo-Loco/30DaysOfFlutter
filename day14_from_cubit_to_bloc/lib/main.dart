7ee8e51888c53821cf9b83fa1fbddf8c6a328642
Kilo Loco <kyle@kiloloco.com>
2021-02-15 21:27:06 -0800

    day14-from-cubit-to-bloc finished

---
+++ day14_from_cubit_to_bloc/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_bloc_and_cubit/posts_cubit.dart';
+import 'package:flutter_bloc_and_cubit/posts_view.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: BlocProvider<PostsBloc>(
+            create: (context) => PostsBloc()..add(LoadPostsEvent()),
+            child: PostsView()));
+  }
+}
---
+++ day14_from_cubit_to_bloc/lib/post.dart
@@ -0,0 +1,14 @@
+class Post {
+  final int userId;
+  final int id;
+  final String title;
+  final String body;
+
+  Post({this.userId, this.id, this.title, this.body});
+
+  factory Post.fromJson(Map<String, dynamic> json) => Post(
+      userId: json['userId'],
+      id: json['id'],
+      title: json['title'],
+      body: json['body']);
+}
