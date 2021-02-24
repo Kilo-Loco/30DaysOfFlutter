013a1085cedb6c2316a8564f09e6d5f9914aa23d
Kilo Loco <kyle@kiloloco.com>
2021-02-23 19:54:23 -0800

    day18-todo-app finished

--- day18_todo_app/day18_todo_app/lib/main.dart
+++ day18_todo_app/day18_todo_app/lib/main.dart
@@ -1,5 +1,13 @@
+import 'package:amplify_datastore/amplify_datastore.dart';
+import 'package:amplify_flutter/amplify.dart';
+import 'package:day18_todo_app/loading_view.dart';
+import 'package:day18_todo_app/todo_cubit.dart';
 import 'package:day18_todo_app/todos_view.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'amplifyconfiguration.dart';
+import 'models/ModelProvider.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,8 +19,34 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  bool _amplifyConfigured = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _configureAmplify();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: TodosView());
+    return MaterialApp(
+        home: BlocProvider(
+            create: (context) => TodoCubit()..getTodos(),
+            child: _amplifyConfigured ? TodosView() : LoadingView()));
+  }
+
+  void _configureAmplify() async {
+    Amplify.addPlugin(AmplifyDataStore(modelProvider: ModelProvider.instance));
+
+    // Once Plugins are added, configure Amplify
+    try {
+      await Amplify.configure(amplifyconfig);
+    } catch (e) {
+      print(e);
+    }
+
+    setState(() {
+      _amplifyConfigured = true;
+    });
   }
 }
